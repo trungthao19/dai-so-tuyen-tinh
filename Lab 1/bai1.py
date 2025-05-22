65c3dbaf5dfd25dc507328a6cfbd4dcd4d3dc264
trungthao19 <trungthao104@gmail.com>
2025-05-22 16:48:40 +0700

    update

---
+++ Lab 1/bai1.py
@@ -0,0 +1,12 @@
+danhsach1 = [1. , 3.] 
+danhsach2 = [5. , 7.] 
+danhsach = danhsach1 + danhsach2 
+print(danhsach)
+danhsach_gapdoi = 2 * danhsach 
+print(danhsach_gapdoi)
+danhsach2 = danhsach * 2 
+print(danhsach2)
+danhsach3 = danhsach / 2
+print(danhsach3)
+#Ghép các danh sách bằng lệnh zip:
+
